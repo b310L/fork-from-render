1bf8a64c44a863564dca89f2b904dd8e679e3d27
b310L <49762348+b310L@users.noreply.github.com>
2023-01-17 09:46:26 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,68 +1,36 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require("cors");
+
 const app = express();
-const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
-app.post('/submit', (req, res) => {
-    console.log('submit clicked');
-    res.writeHead(200,{'content-type':'text/html'});
+app.use(cors());
+// parse application/json
+app.use(bodyParser.json());
+ 
+//create database connection
+// const conn = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'my_db'
+// });
 
-  res.send('<p>submitted</p>')
+// //connect to database
+// conn.connect((err) =>{
+//   if(err) throw err;
+//   console.log('Mysql Connected...');
+// });
 
-    // })
-    
-    
+app.get('/',(req, res) => {
+    res.send("shi")
 })
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+//add new user
+app.post('/submit',(req, res) => {
+  let data = {name: req.body.name};
+  console.log(data);
+});
 
-
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from hossein!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+app.listen(5000, () => {
+  console.log("Server running successfully on 5000");
+});
